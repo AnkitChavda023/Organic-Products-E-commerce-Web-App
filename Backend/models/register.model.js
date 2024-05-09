f1c24efce616f98ec4a3c9265438837b1324a602
Ankit Chavda <ankitchavda8479@gmail.com>
2024-05-09 13:09:55 +0530

    Add backend folder

---
+++ Backend/models/register.model.js
@@ -0,0 +1,33 @@
+// import { lowerCase } from 'lodash';
+// import mongoose from 'mongoose';
+
+const registerDataSchema = new mongoose.Schema(
+    {
+        username : {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email : {
+            type: String,
+            required: [true , "Email address is required"],
+            unique: [true,"Email address already exist!"],
+            lowercase: true
+        },
+        mobile : {
+            type: String,
+            required: [true,"Mobile number is required"],
+            unique: [true,"Mobile number is already exist!"],
+            
+        },
+        password :  {
+            type: String,
+            required: [true,"Password is required"],
+        },
+        confirmPassword : {
+            type: String,
+            required: true,
+        },  
+    }, {timestamps: true});
+
+export const UserData = mongoose.model("UserData" , registerDataSchema);
