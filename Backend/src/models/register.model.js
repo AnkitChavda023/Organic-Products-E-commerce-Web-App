9d479041228801a5f8752545fa4fb7ec684975d5
Ankit Chavda <ankitchavda8479@gmail.com>
2024-05-10 11:08:38 +0530

    add register model

--- Backend/src/models/register.model.js
+++ Backend/src/models/register.model.js
@@ -1,32 +1,72 @@
 import mongoose from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const registerDataSchema = new mongoose.Schema(
     {
         username : {
             type: String,
             required: true,
-            unique: true
+            unique: true,
+            index: true
         },
         email : {
             type: String,
             required: [true , "Email address is required"],
             unique: [true,"Email address already exist!"],
-            lowercase: true
+            lowercase: true,
+            trim: true
         },
         mobile : {
             type: String,
             required: [true,"Mobile number is required"],
             unique: [true,"Mobile number is already exist!"],
-            
         },
         password :  {
             type: String,
             required: [true,"Password is required"],
-        },
-        confirmPassword : {
+        }, 
+        refreshToken : {
             type: String,
             required: true,
-        },  
+            unique: true,
+        },
     }, {timestamps: true});
+
+    // registerDataSchema.plugin(mongooseAggregatePaginate);
+    registerDataSchema.pre("save", async function(next){
+        if(!this.isModified("password")) return next();
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    });
+
+    registerDataSchema.methods.isPasswordCorrect = async function(password){
+       return await bcrypt.compare(password, this.password);
+    }
+
+    registerDataSchema.methods.generateAccessToken =  function(){
+       return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            mobile: this.mobile,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPITY
+        }
+    )
+    }
+    registerDataSchema.methods.generateRefreshToken = function(){
+        return jwt.sign({
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+    }
 
 export const UserData = mongoose.model("UserData" , registerDataSchema);
