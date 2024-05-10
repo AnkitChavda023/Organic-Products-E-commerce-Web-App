cfca2d173dcef0e4aafc6626cdecddc5b71cb64b
Ankit Chavda <ankitchavda8479@gmail.com>
2024-05-10 10:07:50 +0530

    util code  added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,21 @@
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from '../src/app.js';
 dotenv.config({
     path:'./env',
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (err)=>{
+        console.log("ERR: ", err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("server listening on port :" + process.env.PORT);
+    });
+})
+.catch((error)=>{
+    console.log("MongoDB connection Failed!: " + error);
+});
 
---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+
+// first way using thenand catch
+
+/*
+const asyncHandler =  (fn) => { async (req, res,next) => {
+    try {
+        await fn(req, res,next);
+        
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message: error.message
+        })
+    }
+} };
+*/
+
+// second way using promise
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((error) => next(error));
+    }
+};
+
+export {asyncHandler};
+
+
+
