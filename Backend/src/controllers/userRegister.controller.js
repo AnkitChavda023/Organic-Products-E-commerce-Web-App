12ed7d638c0c86f7f90bffc901ea33a96b05010d
Ankit Chavda <ankitchavda8479@gmail.com>
2024-05-10 14:06:03 +0530

    add register controller

--- Backend/src/controllers/userRegister.controller.js
+++ Backend/src/controllers/userRegister.controller.js
@@ -1,9 +1,54 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from"../utils/apiError.js";
+import {UserData} from "../models/register.model.js";
+import {ApiResponse} from "../utils/ApiResponce.js";
+
 
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message: "User exist "
+    const {username , mobile,email, password } = req.body;
+    console.log(username);
+
+    // if(username === ""){
+    //     throw new ApiError(400, "username is required");
+    // }
+    // if(mobile === ""){
+    //     throw new ApiError(400, "username is required");
+    // }
+
+    //alternate method  for condition
+
+    if(
+        [username, mobile, email, password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields must be required");
+    }
+
+    const existedUser = UserData.findOne({
+        $or:[{ username }, { mobile }, { email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with username or emial already exist");
+    }
+
+    const user = await UserData.create({
+        username: username.toLowerCase(),
+        mobile,
+        email,
+        password,
     });
+
+    const createdUser = await UserData.findById(user.id).select(
+        "-password -refreshToken"
+    );
+
+    if(createdUser){
+        throw new ApiError(500, "someting want wrong when registering a user" );
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, "User Register successfully...", createdUser)
+    )
 });
 
 
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/userRegister.controller.js";
-
+import {upload} from "../middlewares/multer.middeleware.js"
 const router = Router();
 
 router.route("/register").post(registerUser)
